4e81d5e64ecd37549b363e8788f3825566f4bd86
Chris Soyars <ctso@ctso.me>
2010-12-23 00:44:33 +0100

    Add otapackage support for backuptool
    Change-Id: I512554c579d444067cd3ccbb0e6946a5eb6bc964a
    
    Conflicts:
    	core/Makefile
    	tools/releasetools/edify_generator.py
    	tools/releasetools/ota_from_target_files

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -170,6 +170,11 @@ class EdifyGenerator(object):
         ## Hax: a failure from run_program doesn't trigger an abort, so have it change the key value and check for "INVALID"
         self.script.append('sha1_check(read_file("/tmp/releasekey"),"7241e92725436afc79389d4fc2333a2aa8c20230") && abort("Can\'t install this package on top of incompatible data. Please try another package or run a factory reset");')
 
+  def RunBackup(self, command):
+    self.script.append('package_extract_file("system/bin/backuptool.sh", "/tmp/backuptool.sh");')
+    self.script.append('set_perm(0, 0, 0777, "/tmp/backuptool.sh");')
+    self.script.append(('run_program("/tmp/backuptool.sh", "%s");' % command))
+
   def ShowProgress(self, frac, dur):
     """Update the progress bar, advancing it over 'frac' over the next
     'dur' seconds.  'dur' may be zero to advance it via SetProgress
