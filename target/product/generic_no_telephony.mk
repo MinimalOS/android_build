79ecf61383480b8ff74b5e8ae22471a532aab73d
iceandfire <arhamjamal@gmail.com>
2012-11-17 02:28:27 +0530

    Busubox, bash, rsync, etc added.
    Change-Id: I83d8056657ee16f8e5587418288d16e1098ae137
    Signed-off-by: iceandfire <arhamjamal@gmail.com>
    
    Conflicts:
    	target/product/generic_no_telephony.mk

--- target/product/generic_no_telephony.mk
+++ target/product/generic_no_telephony.mk
@@ -21,7 +21,6 @@ PRODUCT_PACKAGES := \
     Bluetooth \
     Camera2 \
     Gallery2 \
-    Music \
     MusicFX \
     OneTimeInitializer \
     Provision \
@@ -33,6 +32,14 @@ PRODUCT_PACKAGES += \
     clatd.conf \
     pppd \
     screenrecord
+
+PRODUCT_PACKAGES += \
+    busybox \
+    rsync \
+    libncurses \
+    nano \
+    lsof \
+    bash
 
 PRODUCT_PACKAGES += \
     librs_jni \
