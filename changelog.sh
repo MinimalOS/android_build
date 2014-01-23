9ca560ce5fe10ec6d632db126cc5af9d5c3489f0
Kyler <mbqsniper@gmail.com>
2014-01-23 12:00:50 -0700

    changelog.sh:
    * Clean up (a minor one)
    * Remove XenonChangelogs in the Xenon folder and move it to build (derp)
    * chmod 777 -R juuust in case
    * Other stuff not really worth mentioning.

--- changelog.sh
+++ changelog.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-# To run this, simply cd to the xenon folder and run: . build/changelog.sh :)
+# To run this, simply cd to the xenon folder and run: 
+
+# . build/changelog.sh :)
 
 _now=$(date +"%m-%d-%Y")
-_file=XenonChangelogs/$_now/Xenon-Changelog-$_now.log
+_file=build/XenonChangelogs/$_now/Xenon-Changelog-$_now.log
 
-mkdir -p XenonChangelogs/ && mkdir -p XenonChangelogs/$_now # && cd /home/kyler/Desktop/xenon/build/ &&
+rm -rf XenonChangelogs 
+
+mkdir -p build/XenonChangelogs/
+
+mkdir -p build/XenonChangelogs/$_now
+
+chmod 777 -R build/XenonChangelogs
 
 repo forall -pc git log --reverse --no-merges --since=1.day.ago >  $_file
