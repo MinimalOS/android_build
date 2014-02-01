a516125c65ca329fe2801d8b5f94b8ad1b6e1706
Kyler <mbqsniper@gmail.com>
2014-01-31 19:35:27 -0700

    revert my last 2 commits for now

--- changelog.sh
+++ changelog.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-# To run this, simply cd to the xenon folder and run build/changelog.sh :)
+# To run this, simply cd to the xenon folder and run: 
+
+# . build/changelog.sh :)
 
 _now=$(date +"%m-%d-%Y")
 _file=build/XenonChangelogs/$_now/Xenon-Changelog-$_now.log
 
-if [ -d XenonChangelogs ]; then
-rm -rf XenonChangelogs
-fi
+rm -rf XenonChangelogs 
 
-if [ -d build/XenonChangelogs ]; then
 mkdir -p build/XenonChangelogs/
-fi
 
 mkdir -p build/XenonChangelogs/$_now
 
