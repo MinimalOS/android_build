ac5d551e184bcc8909c160a6d5ae49d7c3ab6ca0
Matt Mower <mowerm@gmail.com>
2014-08-28 16:51:02 -0500

    Edify: Add AssertSomeBaseband
    Allow releasetools.py to assert a baseband version. Works just like
    AssertSomeBootloader.
    
    Change-Id: Ic8eb341cef1d777d983be25ba21a3bc545819c29

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -132,6 +132,14 @@ class EdifyGenerator(object):
            ");")
     self.script.append(self._WordWrap(cmd))
 
+  def AssertSomeBaseband(self, *basebands):
+    """Assert that the baseband version is one of *basebands."""
+    cmd = ("assert(" +
+           " ||\0".join(['getprop("ro.baseband") == "%s"' % (b,)
+                         for b in basebands]) +
+           ");")
+    self.script.append(self._WordWrap(cmd))
+
   def RunBackup(self, command):
     self.script.append('package_extract_file("system/bin/backuptool.sh", "/tmp/backuptool.sh");')
     self.script.append('set_perm(0, 0, 0777, "/tmp/backuptool.sh");')
