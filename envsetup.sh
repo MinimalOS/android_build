d653f9069703c30743ed86b7adcb4c85be47522e
iceandfire <arhamjamal@gmail.com>
2014-11-08 12:27:58 +0530

    Add mka function
    Credits:CM
    
    https://github.com/CyanogenMod/android_build/commit/a254921e2cdc5128895e21b3720e7427d6cf4cb9
    
    Signed-off-by: iceandfire <arhamjamal@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -16,6 +16,7 @@ Invoke ". build/envsetup.sh" from your shell to add the following functions to y
 - resgrep: Greps on all local res/*.xml files.
 - sgrep:   Greps on all local source files.
 - godir:   Go to the directory containing a file.
+- mka:     Builds using SCHED_BATCH on all processors
 
 Look at the source to view more functions. The complete list is:
 EOF
@@ -1451,6 +1452,17 @@ function godir () {
         pathname=${lines[0]}
     fi
     \cd $T/$pathname
+}
+
+function mka() {
+    case `uname -s` in
+        Darwin)
+            make -j `sysctl hw.ncpu|cut -d" " -f2` "$@"
+            ;;
+        *)
+            schedtool -B -n 1 -e ionice -n 1 make -j `cat /proc/cpuinfo | grep "^processor" | wc -l` "$@"
+            ;;
+    esac
 }
 
 # Force JAVA_HOME to point to java 1.7 or java 1.6  if it isn't already set.
