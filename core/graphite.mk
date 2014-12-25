a7080107cf4c9645b10c75a8b5771d26fde0a5ab
frap129 <frap129@gmail.com>
2014-12-24 23:08:45 -0500

    Continue Optimizations
    Seperate graphite optimizations from select.mk to prevent errors with non-compatible toolchains, enable flags based on electrify, not on toolchain version, and add more info to dumpvar.mk.
    
    Signed-off-by: frap129 <frap129@gmail.com>

--- core/graphite.mk
+++ core/graphite.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 #
 
-GRAPHITE_FLAGS := -fgraphite -floop-flatten -floop-parallelize-all -ftree-loop-linear -floop-interchange -floop-strip-mine -floop-block
+GRAPHITE_FLAGS := $(call cc-option,$(-fgraphite -floop-flatten -floop-parallelize-all -ftree-loop-linear -floop-interchange -floop-strip-mine -floop-block))
 
 # Only use graphite if using SaberMod toolchain.  Allow any version of SaberMod with the wildcard symbol.
 ifneq ($(filter (SaberMod%), $(shell $(TARGET_CC) --version)),)
@@ -24,7 +24,7 @@ LOCAL_CFLAGS := $(GRAPHITE_FLAGS)
 endif
 
 ifdef LOCAL_CPPFLAGS
-LOCAL_CFLAGS += $(GRAPHITE_FLAGS)
+LOCAL_CPPFLAGS += $(GRAPHITE_FLAGS)
 else
 LOCAL_CPPFLAGS := $(GRAPHITE_FLAGS)
 endif
@@ -51,6 +51,24 @@ ifdef $(combo_2nd_arch_prefix)TARGET_RELEASE_CPPFLAGS
 $(combo_2nd_arch_prefix)TARGET_RELEASE_CPPFLAGS += $(GRAPHITE_FLAGS)
 else
 $(combo_2nd_arch_prefix)TARGET_RELEASE_CPPFLAGS := $(GRAPHITE_FLAGS)
+endif
+
+ifdef $(combo_var_prefix)GLOBAL_CFLAGS
+$(combo_var_prefix)GLOBAL_CFLAGS += $(GRAPHITE_FLAGS)
+else
+$(combo_var_prefix)GLOBAL_CFLAGS := $(GRAPHITE_FLAGS)
+endif
+
+ifdef $(combo_var_prefix)RELEASE_CFLAGS
+$(combo_var_prefix)RELEASE_CFLAGS += $(GRAPHITE_FLAGS)
+else
+$(combo_var_prefix)RELEASE_CFLAGS := $(GRAPHITE_FLAGS)
+endif 
+
+ifdef $(combo_var_prefix)GLOBAL_CPPFLAGS
+$(combo_var_prefix)GLOBAL_CPPFLAGS += $(GRAPHITE_FLAGS)
+else
+$(combo_var_prefix)GLOBAL_CPPFLAGS := $(GRAPHITE_FLAGS)
 endif
 endif
 #####
