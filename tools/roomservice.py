bb14110c5ec68585f51aef204ca3df8084b5a869
TheBr0ken <jtoro2716@gmail.com>
2015-02-11 16:44:05 -0600

    Update roomservice defualt branch

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -36,7 +36,7 @@ except ImportError:
 # set this to the default remote to use in repo
 default_rem = "github"
 # set this to the default revision to use (branch/tag name)
-default_rev = "kitkat"
+default_rev = "lollipop"
 # set this to the remote that you use for projects from your team repos
 # example fetch="https://github.com/TeamHorizon"
 default_team_rem = "TeamHorizon"
