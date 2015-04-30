c6ed026ed6a1ebd4bb4462d3c6bbc9d79c7364a7
Joe Maples <frap129@gmail.com>
2015-04-30 15:44:13 -0400

    Fix build on Ubuntu 15.04 Vivid Vervet
    Java is no longer detected as being the correct version in Vivid Vervet, fix the syntax of the Java and Javac check to fix this
    
    Signed-off-by: Joe Maples <frap129@gmail.com>

--- core/main.mk
+++ core/main.mk
@@ -148,13 +148,13 @@ javac_version_str := $(shell unset _JAVA_OPTIONS && javac -version 2>&1)
 ifeq ($(LEGACY_USE_JAVA6),)
 required_version := "1.7.x"
 required_javac_version := "1.7"
-java_version := $(shell echo '$(java_version_str)' | grep '^java .*[ "]1\.7[\. "$$]')
-javac_version := $(shell echo '$(javac_version_str)' | grep '[ "]1\.7[\. "$$]')
+java_version := $(shell echo '$(java_version_str)' | grep '^java .*[ "]1\.7[\.*_"$$]')
+javac_version := $(shell echo '$(javac_version_str)' | grep '[ "]1\.7[\.*_"$$]')
 else # if LEGACY_USE_JAVA6
 required_version := "1.6.x"
 required_javac_version := "1.6"
-java_version := $(shell echo '$(java_version_str)' | grep '^java .*[ "]1\.6[\. "$$]')
-javac_version := $(shell echo '$(javac_version_str)' | grep '[ "]1\.6[\. "$$]')
+java_version := $(shell echo '$(java_version_str)' | grep '^java .*[ "]1\.6[\.*_ "$$]')
+javac_version := $(shell echo '$(javac_version_str)' | grep '[ "]1\.6[\.*_ "$$]')
 endif # if LEGACY_USE_JAVA6
 
 ifeq ($(strip $(java_version)),)
